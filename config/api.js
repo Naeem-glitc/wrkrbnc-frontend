2e4f79c14bfeb778cb18045b3b9b66020bf766a0
Naeem Abbas <naeem401080@gmail.com>
2025-12-07 23:04:39 +0500

    update

--- config/api.js
+++ config/api.js
@@ -1,6 +1,6 @@
 
 const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://wrkrbnc-backend.vercel.app'  
+  ? 'https://wrkrbnc-backend.vercel.app/'  
   : 'http://localhost:3001';               
 
 export default API_BASE_URL;
